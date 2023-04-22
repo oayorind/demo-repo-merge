13d95c4553a761ec1b354e7921a56b45a6d1f26f
Oyewole Ayorinde <OyeAyorinde>
2023-04-22 18:16:24 -0400

    pull request demo

---
+++ API/rest-api.tf
@@ -0,0 +1,37 @@
+resource "aws_api_gateway_rest_api" "example_api" {
+  name = "example-api"
+}
+
+resource "aws_api_gateway_resource" "example_resource" {
+  rest_api_id = aws_api_gateway_rest_api.example_api.id
+  parent_id   = aws_api_gateway_rest_api.example_api.root_resource_id
+  path_part   = "example"
+}
+
+resource "aws_api_gateway_method" "example_method" {
+  rest_api_id   = aws_api_gateway_rest_api.example_api.id
+  resource_id   = aws_api_gateway_resource.example_resource.id
+  http_method   = "GET"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "example_integration" {
+  rest_api_id             = aws_api_gateway_rest_api.example_api.id
+  resource_id             = aws_api_gateway_resource.example_resource.id
+  http_method             = aws_api_gateway_method.example_method.http_method
+  integration_http_method = "GET"
+  type                    = "HTTP"
+  uri                     = "http://example.com/api"
+}
+
+resource "aws_api_gateway_method_response" "example_method_response" {
+  rest_api_id = aws_api_gateway_rest_api.example_api.id
+  resource_id = aws_api_gateway_resource.example_resource.id
+  http_method = aws_api_gateway_method.example_method.http_method
+  status_code = "200"
+}
+
+resource "aws_api_gateway_deployment" "example_deployment" {
+  rest_api_id = aws_api_gateway_rest_api.example_api.id
+  stage_name  = "prod"
+}
